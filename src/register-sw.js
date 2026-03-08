fac1cdcbe168d3c7e5a46d239eff21e9edb23d33
Gwenaël <votre-email@exemple.com>
2026-03-08 21:31:06 +0100

    rename sw to firebase-messaging-sw

--- src/register-sw.js
+++ src/register-sw.js
@@ -1,9 +1,7 @@
 // src/register-sw.js
-// À importer dans main.jsx : import './register-sw.js'
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/' })
       .then(reg => console.log('[SW] registered:', reg.scope))
       .catch(err => console.error('[SW] registration failed:', err))
   })
